2f99acd1a569df10d8a46898b58d5d999042f9e9
edward <edwardlfish@gmail.com>
2024-11-02 12:38:36 +0200

    Receive from eddpulsedata

--- src/main/java/ait/cohort46/pulse/service/PulseService.java
+++ src/main/java/ait/cohort46/pulse/service/PulseService.java
@@ -1,18 +1,14 @@
 package ait.cohort46.pulse.service;
 
 import ait.cohort46.pulse.dto.PulseDto;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Consumer;
 
 @Service
-@RequiredArgsConstructor
 public class PulseService {
-    private final StreamBridge streamBridge;
     @Value("${pulse.min}")
     private int minPulse;
     @Value("${pulse.max}")
@@ -21,16 +17,10 @@ public class PulseService {
     @Bean
     public Consumer<PulseDto> dispatchData() {
         return p -> {
-            if (p.getPayload() < minPulse) {
-                streamBridge.send("lowpulse-out-0", p);
-                return;
+            if (p.getPayload() >= minPulse && p.getPayload() <= maxPulse) {
+                long delay = System.currentTimeMillis() - p.getTimestamp();
+                System.out.println("delay: " + delay + ", id: " + p.getId() + ", pulse: " + p.getPayload());
             }
-            if (p.getPayload() > maxPulse) {
-                streamBridge.send("highpulse-out-0", p);
-                return;
-            }
-            long delay = System.currentTimeMillis() - p.getTimestamp();
-            System.out.println("delay: " + delay + ", id: " + p.getId() + ", pulse: " + p.getPayload());
         };
     }
 }
